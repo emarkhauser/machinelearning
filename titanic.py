58b7201d5f5dad328632d90a52acc70df6a57e15
Erik Markhauser <emarkhauser@yahoo.com>
2018-04-02 19:55:56 -0400

    Delete files that were mistakenly committed

--- titanic.py
+++
@@ -1,7 +0,0 @@
-import machinelearning
-
-ml = machinelearning.MachineLearning('train.csv')
-ml.cleanIntervalData('Age')
-ml.prepareData(['Pclass','Sex','SibSp','Parch','Embarked','Age'], 'Survived')
-ml.runAlgorithms()
-
